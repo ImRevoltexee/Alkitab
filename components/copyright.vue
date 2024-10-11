a7648a6bed68e884bf76366de6b407bdaa5818d0
Revolt <118598395+ImRevoltexee@users.noreply.github.com>
2024-10-11 11:01:53 +0700

    Update copyright.vue

--- components/copyright.vue
+++ components/copyright.vue
@@ -2,13 +2,13 @@
 	<div class="flex flex-col items-center">
 		<div class="w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 mt-12">
 			<div class="border-t py-6 text-sm">
-				⓪ 2023
+				⓪ 2024
 				<NuxtLink
-					to="https://erwindosianipar.github.io"
+					to="https://github.com/imrevoltexee"
 					class="underline"
-					>Erwindo Sianipar</NuxtLink
+					>Revolt</NuxtLink
 				>
-				/ No rights reserved by the author / v1.0.0 / Soli Deo gloria
+				/Source Code Writer By Erwindo Sianipar
 			</div>
 		</div>
 	</div>
